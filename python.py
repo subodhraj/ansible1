5c9ce8af427ec66d7ad6f14a3d395db8fe856880
bajrasubodh <bajrasubodh@gmail.com>
2020-10-14 23:54:33 -0400

    changes

---
+++ python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+myname = "subodh"
+lname = "chutya"
+print(myname)
+print(myname.capitalize())
+print(myname.upper())
+print(myname + " " + lname)
+
+users = ["user1","user2","user3"]
+
+print(users[1])
+print(users[-1])
+users.append("ram")
+print("users" + users[-1])
+users.insert(1,"marry")
+users.pop
+sorter_users = sorted(users)
+print(sorter_users)
+
+#loop
+
+for u in users:
+    

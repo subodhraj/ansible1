bd0afcc71f170fec9a33dcceef7f457d54b9e567
bajrasubodh <bajrasubodh@gmail.com>
2020-10-18 03:27:30 -0400

    king

--- newfunction.py
+++ newfunction.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 manche = [
-    {"name":"subodh1","job":"system","age":"31"},
+    {"name":"subodh","job":"system","age":"31"},
     {"name":"pashu","job":"qa","age":"34"},
     {"name":"kari","job":"ba","age":"26"}
 ]
@@ -10,3 +10,13 @@ def auth(name1,job1):
         if u['name'] == name1 and u['job'] == job1:
            return True
     return False
+
+auth('ram','shyam')
+
+k = input("enter a name: ")
+c = input("enter a job: ")
+
+if auth('k','c'):
+    print("welcome to the page my lord, you are the king of the universe")
+else:
+    print("fuck off mother fucker go back to your country")

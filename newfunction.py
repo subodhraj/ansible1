774baa2e68c28e39f36cd4dbd9436b39e6abf89d
bajrasubodh <bajrasubodh@gmail.com>
2020-10-18 03:55:56 -0400

    changes

--- newfunction.py
+++ newfunction.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 manche = [
-    {"name":"subodh","job":"system","age":"34"},
-    {"name":"pashu","job":"qa","age":"34"},
-    {"name":"ajay","job":"ba","age":"26"}
+    {"name":"subodh","job":"system","age":"34","password":"nepal123"},
+    {"name":"pashu","job":"qa","age":"34","password":"pashu123"},
+    {"name":"ajay","job":"ba","age":"26","password":"ajay123"}
 ]
 
-def auth(name1,job1):
+def auth(name1,job1,pass1):
     for u in manche:
-        if u['name'] == name1 and u['job'] == job1:
+        if u['name'] == name1 and u['job'] == job1 and u['pass'] == pass1:
             return True
     return False
 
 k = input("enter a name: ")
 c = input("enter a job: ")
+d = input("enter a password: ")
+
 print(k)
 print(c)
 
-if auth(k,c):
+if auth(k,c,d):
     print("welcome to the page my lord, you are the king of the universe")
 else:
     print("fuck off mother fucker, go back to your country")
